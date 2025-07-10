988a2a955b07dbdc5ac305dafe80d1ad8c8afc1e
flavioabdon <flaviocondori@gmail.com>
2025-07-09 21:11:27 -0400

    Subir cambios

--- app.js
+++ app.js
@@ -12,7 +12,6 @@ app.set('view engine', 'ejs');
 app.use(expressLayouts);
 app.set('layout', 'layout'); 
 
-
 // Middlewares
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -36,10 +35,10 @@ app.set('view engine', 'ejs');
 app.use((req, res, next) => {
     res.locals.success_msg = req.flash('success_msg');
     res.locals.error_msg = req.flash('error_msg');
-    res.locals.info_msg = req.flash('info_msg'); // Añade esta línea
+    res.locals.info_msg = req.flash('info_msg');
     res.locals.user = req.session.user || null;
     next();
-  });
+});
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/css', express.static(path.join(__dirname, 'public/css')));
@@ -54,7 +53,21 @@ app.use('/', authRoutes);
 app.use('/admin', adminRoutes);
 app.use('/admin/users', userRoutes);
 
-// Iniciar servidor
-app.listen(port, () => {
+// Iniciar servidor en todas las interfaces
+app.listen(port, '0.0.0.0', () => {
   console.log(`Servidor corriendo en http://localhost:${port}`);
+  console.log(`Accesible en la red local en: http://[tu-ip-local]:${port}`);
+  
+  // Opcional: Mostrar las interfaces de red disponibles
+  const os = require('os');
+  const interfaces = os.networkInterfaces();
+  
+  console.log('\nDirecciones IP disponibles:');
+  Object.keys(interfaces).forEach(iface => {
+    interfaces[iface].forEach(details => {
+      if (details.family === 'IPv4' && !details.internal) {
+        console.log(`- http://${details.address}:${port}`);
+      }
+    });
+  });
 });
